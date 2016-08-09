31d3d79e843f31e82a6f70955ce1c037cf58f5df
sadams <sadams@redhat.com>
2016-08-09 16:03:14 -0400

    Add XP modifier and correct Gold modifier

--- functions.py
+++ functions.py
@@ -17,7 +17,9 @@ def displaySheet():
         print(k + ': ' + str(v))
 
 def modGold(num):
-    if num <= 0:
+    if inventory['Gold'] == 0 and num <= 0:
+        print('You don\'t have any money to lose.')
+    elif num <= 0:
         inventory['Gold'] -= num
     else:
         inventory['Gold'] += num
@@ -27,3 +29,6 @@ def clearScreen():
         os.system('cls')
     else:
         os.system('clear')
+
+def modXP(num):
+    character['XP'] += num
