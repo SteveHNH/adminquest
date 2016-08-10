21d2c5a522c269fa43aadaffcbc2ac1a2ebaa11e
sadams <sadams@redhat.com>
2016-08-09 22:15:48 -0400

    Add ability to change data after creating character

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@
 Common functions
 """
 from characterFunctions import character, inventory
+from questBoard import availableQuests
 import os
 
 def pressEnter():
@@ -13,8 +14,8 @@ def speech(text):
     print('')
 
 def displaySheet():
-    for k, v in character.iteritems():
-        print(k + ': ' + str(v))
+    for key in character:
+        print(key + ': ' + str(character[key]).title())
 
 def modGold(num):
     if inventory['Gold'] == 0 and num <= 0:
@@ -32,3 +33,22 @@ def clearScreen():
 
 def modXP(num):
     character['XP'] += num
+
+def mainTitle():
+    print('''
+    _       _           _        ___                  _
+   / \   __| |_ __ ___ (_)_ __  / _ \ _   _  ___  ___| |_
+  / _ \ / _` | '_ ` _ \| | '_ \| | | | | | |/ _ \/ __| __|
+ / ___ \ (_| | | | | | | | | | | |_| | |_| |  __/\__ \ |_
+/_/   \_\__,_|_| |_| |_|_|_| |_|\__\_\\__,_|\___||___/\__|
+==========================================================
+        A System Administration Learning Adventure
+        ''')
+
+def printQuests():
+    print('''
+          QUEST BOARD
+          ===========
+          ''')
+    for k in availableQuests:
+        print(k + ': ' + availableQuests[k])
--- launch.py
+++ launch.py
@@ -8,21 +8,14 @@ idea what I'm doing yet, but I'm kind of excited about the possibilities.
 from __future__ import print_function
 from questBoard import availableQuests
 from characterFunctions import inventory, character, availableRaces, availableClasses
-from functions import pressEnter, speech, modGold, displaySheet, clearScreen
+from functions import printQuests, pressEnter, speech, modGold, displaySheet, clearScreen, mainTitle
 
-def printQuests():
-    print('''
-          QUEST BOARD
-          ===========
-          ''')
-    for k in availableQuests:
-        print(k + ': ' + availableQuests[k])
+mainTitle()
 
-print('''
-                 ADMINQUEST
-                 ==========
-       A Sysadmin Learning Adventure!
-       ''')
+print('1. Begin Quest')
+print('')
+print('Choice: ', end='')
+choice = raw_input()
 
 print('What is your name, Adventurer? ')
 character['NAME'] = raw_input()
@@ -98,12 +91,71 @@ speech('Oh! By the way, here\'s ' + str(inventory['Gold']) +
 
 pressEnter()
 clearScreen()
+speech('Makes sure I got all your details correct')
+displaySheet()
+
+# Changing stuff is currently broken. Something up with the while loop
+speech('Would you like to change anything? (y/n)')
+sheetChange = raw_input().lower()
+if sheetChange == 'y':
+    while sheetChange == 'y':
+        speech('What would you like to change?')
+        print('Name, Class, or Race: ', end='')
+        choice = raw_input().lower
+        if choice == 'name':
+            speech('What would you like to change your name to?')
+            character['NAME'] = ''
+            while character['NAME'] == '':
+                newName = raw_input().lower
+                character['NAME'] = newName
+            speech('Name changed to ' + character['Name'])
+            print('Would you like to change anything else? (y/n)', end='')
+            answer = raw_input().lower()
+            if answer == 'n':
+                sheetChange = 'n'
+        elif choice == 'class':
+            speech('What would you like to change your class to?')
+            character['CLASS'] = ''
+            while character['CLASS'] == '':
+                newClass = raw_input().lower
+                character['CLASS'] = newClass
+                if character['CLASS'] not in availableClasses:
+                    print('Not a valid class')
+                    print('Choose one of the following')
+                    for classes in availableClasses:
+                        print(classes.title())
+                    character['CLASS'] = ''
+            print('Would you like to change anything else? (y/n)', end='')
+            answer = raw_input().lower()
+            if answer == 'n':
+                sheetChange = 'n'
+        elif choice == 'race':
+            speech('What would you like to change your class to?')
+            character['RACE'] = ''
+            while character['RACE'] == '':
+                newRace = raw_input().lower
+                character['RACE'] = newRace
+                if character['RACE'] not in availableRaces:
+                    print('Not a valid class')
+                    print('Choose one of the following')
+                    for race in availableRaces:
+                        print(race.title())
+                    character['RACE'] = ''
+            print('Would you like to change anything else? (y/n)', end='')
+            answer = raw_input().lower()
+            if answer == 'n':
+                sheetChange = 'n'
+        else:
+            print('If you want to change something, do it.')
+
+
 speech('One more thing, ' +character['NAME'] + '.')
 pressEnter()
 speech('It\'s dangerous to go alone. Take this!')
 inventory['Mainhand'] = 'Wooden Stick'
 speech('Received: ' + inventory['Mainhand'])
 pressEnter()
+clearScreen()
 
-displaySheet()
+
 printQuests()
